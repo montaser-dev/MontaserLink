f44a60cc5adb698a02054737f4891336e77a68b2
montaser-dev <abdallahelmontaser786@gmail.com>
2024-02-16 23:05:44 +0100

    Add CRUD operations for Posts

--- src/components/shared/Loader.tsx
+++ src/components/shared/Loader.tsx
@@ -1,14 +1,14 @@
-const Loader = () => {
-    return (
-        <div className="flex-center w-full">
-            <img 
-            src="/public/assets/icons/loader.svg" 
-            alt="loader"
-            width={24}
-            height={24}
-            />  
-        </div>
-    )
-}
-
-export default Loader
+const Loader = () => (
+    <div className="flex-center w-full">
+      <img
+        src="/assets/icons/loader.svg"
+        alt="loader"
+        width={24}
+        height={24}
+        className="animate-spin"
+      />
+    </div>
+  );
+  
+  export default Loader;
+  
